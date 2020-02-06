d871c9cb45b8c81fc9810b14d995c4d06b25e40e
zefe5675 <43979810+zefe5675@users.noreply.github.com>
2020-02-06 14:22:38 -0700

    Add files via upload

---
+++ practice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author : Your Name
+# Date: Today's date
+# Script follows here:
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$(($numOne + $numTwo))
+echo "The sum is : $sum"
+let prod=numOne*numTwo
+echo "The product is: $prod"
